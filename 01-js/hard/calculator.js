e48cade7d0c5246194955706b5a6a1fdbb96f9af
Puneet Uttam <puneet.uttam.99@gmail.com>
2024-06-10 01:42:15 +0530

    Calculator Sol

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -10,12 +10,59 @@
     - calculate: takes a string expression which can take multi-arithmetic operations and give its result
       example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
       Points to Note: 
-        1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
+        1. the input can  have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
         2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
 
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(num) {
+    if (typeof num === "number") {
+      this.result += num;
+    } else {
+      throw new Error("Invalid Number");
+    }
+  }
+  subtract(num) {
+    if (typeof num === "number") {
+      this.result -= num;
+    } else {
+      throw new Error("Invalid Number");
+    }
+  }
+  multiply(num) {
+    if (typeof num === "number") {
+      this.result *= num;
+    } else {
+      throw new Error("Invalid Number");
+    }
+  }
+  divide(num) {
+    if (typeof num === "number" && num !== 0) {
+      this.result /= num;
+    } else {
+      throw new Error("Invalid Number");
+    }
+  }
+  clear() {
+    this.result = 0;
+  }
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression){
+    this.result=eval(expression)
+    if(this.result===Infinity){
+      throw new Error("Divided by zero")
+    }
+    return this.result;
+  }
+}
 
 module.exports = Calculator;
