a531fdbf38737a9b11e5e493a379da17526f4e51
Puneet Uttam <puneet.uttam.99@gmail.com>
2024-06-10 00:10:28 +0530

    Anagram Sol

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,26 @@
 */
 
 function isAnagram(str1, str2) {
+  const dummy = new Array(256);
 
+  if (str1.length != str2.length) {
+    return false;
+  }
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  for (let i = 0; i < 256; i++) {
+    dummy[i] = 0;
+  }
+  for (let i = 0; i < str1.length; i++) {
+    dummy[str1[i].charCodeAt(0)]++;
+    dummy[str2[i].charCodeAt(0)]--;
+  }
+  for (let i = 0; i < 256; i++) {
+    if (dummy[i] != 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
